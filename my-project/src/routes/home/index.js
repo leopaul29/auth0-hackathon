c0a1c203e165f609ca13b66ab7e5231dc99915c7
Leo-paul <leopaulmartin@hotmail.fr>
2021-08-28 23:49:28 +0200

    trying to display the quizz

--- my-project/src/routes/home/index.js
+++ my-project/src/routes/home/index.js
@@ -1,10 +1,16 @@
 import { h } from 'preact';
 import style from './style.css';
+import quizz from '../../openquizzdb_175.json'
+
+const test = () => {
+	console.log(test)
+}
 
 const Home = () => (
 	<div class={style.home}>
 		<h1>Home</h1>
 		<p>This is the Home component.</p>
+		{test()}
 	</div>
 );
 
