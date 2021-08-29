6d3f45b3ea8c964762b2dd53165045ded1019766
Leo-paul <leopaulmartin@hotmail.fr>
2021-08-29 22:25:53 +0200

    App Auth0 integration

--- my-project/src/components/app.js
+++ my-project/src/components/app.js
@@ -1,21 +1,31 @@
-import { h } from 'preact';
-import { Router } from 'preact-router';
+import { h } from "preact";
+import { Router } from "preact-router";
 
-import Header from './header';
+import Header from "./header";
 
 // Code-splitting is automated for `routes` directory
-import Home from '../routes/home';
-import Profile from '../routes/profile';
+import Home from "../routes/home";
+import Profile from "../routes/profile";
+
+import React from "react";
+import ReactDOM from "react-dom";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 const App = () => (
-	<div id="app">
-		<Header />
-		<Router>
-			<Home path="/" />
-			<Profile path="/profile/" user="me" />
-			<Profile path="/profile/:user" />
-		</Router>
-	</div>
-)
+  <Auth0Provider
+    domain="yrladd.eu.auth0.com"
+    clientId="Nnqj6YR5YjiBDSO6LGlzla1lwMsFs67Y"
+    redirectUri={window.location.origin}
+  >
+    <div id="app">
+      <Header />
+      <Router>
+        <Home path="/" />
+        <Profile path="/profile/" user="me" />
+        <Profile path="/profile/:user" />
+      </Router>
+    </div>
+  </Auth0Provider>
+);
 
 export default App;
